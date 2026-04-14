d49967718f977f64e197103e475015f82d34ee98
sabinashrestha <sabrinashrestha352@gmail.com>
2026-04-14 17:58:10 +0545

    sync latest local changes

--- artifacts/api-server/dist/pino-file.mjs
+++ artifacts/api-server/dist/pino-file.mjs
@@ -4118,7 +4118,7 @@ var require_pino = __commonJS({
     function pinoBundlerAbsolutePath(p) {
       try {
         const path = __require("path");
-        const outputDir = "C:\\Users\\Galaxy\\Desktop\\Clip-Scout-works-1\\artifacts\\api-server\\dist";
+        const outputDir = "C:\\Users\\Galaxy\\Clip-Scout-works-3\\artifacts\\api-server\\dist";
         return path.resolve(outputDir, p.replace(/^\.\//, ""));
       } catch (e) {
         const f = new Function("p", "return new URL(p, import.meta.url).pathname");
